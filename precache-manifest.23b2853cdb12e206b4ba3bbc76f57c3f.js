c0c9275f0615d8c6b296994900a238bd372efb8a
dgk3593 <dgk3593@gmail.com>
2020-09-01 11:47:32 +0700

    Updates

---
+++ precache-manifest.23b2853cdb12e206b4ba3bbc76f57c3f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9479c87a3cea7221ec3c85e6c33020c0",
+    "url": "/dl-model/index.html"
+  },
+  {
+    "revision": "f37d67e4d42987dd0a11",
+    "url": "/dl-model/static/css/main.736614c9.chunk.css"
+  },
+  {
+    "revision": "24973eaa46bc2a0c98a1",
+    "url": "/dl-model/static/js/2.c799f968.chunk.js"
+  },
+  {
+    "revision": "2ced4e3f11ddcff4e4ee342c4f8dee28",
+    "url": "/dl-model/static/js/2.c799f968.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f37d67e4d42987dd0a11",
+    "url": "/dl-model/static/js/main.101764e0.chunk.js"
+  },
+  {
+    "revision": "13eba3f3674c59b595b0",
+    "url": "/dl-model/static/js/runtime-main.88949c46.js"
+  },
+  {
+    "revision": "1911e7786d46c7f6156e8398ee2125fb",
+    "url": "/dl-model/static/media/Rose-Petals.1911e778.svg"
+  }
+]);
