405ef21e0968ea26a2fc4c514b4f76b49a40e027
dgk3593 <dgk3593@gmail.com>
2020-09-17 16:12:16 +0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/dl-model/precache-manifest.dc20151269af4ffb481cd2cb5a001ccd.js"
+  "/dl-model/precache-manifest.dad2e1b34598fe7a7633e5ab7180aa8c.js"
 );
 
 self.addEventListener('message', (event) => {
