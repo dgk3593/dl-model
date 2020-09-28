c33fce92915dae68de98ca6b62221593e3dbe57b
dgk3593 <dgk3593@gmail.com>
2020-09-28 10:16:02 +0700

    Manacaster names

--- src/ModelViewer.js
+++ src/ModelViewer.js
@@ -22,6 +22,7 @@ class ModelViewer extends Component {
             this.mixers.forEach(mixer => mixer.update(dt));
         }
         this.renderer.render(this.scene, this.camera);
+        console.log(this.renderer.info.render.triangles);
     };
 
     // set up initial scene
