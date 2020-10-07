f004761a17a4bb69ff12ab17075513e4b91cf4a0
dgk3593 <dgk3593@gmail.com>
2020-10-07 16:55:32 +0700

    Allies and Enemies models select panel styling

--- src/SimpleOptionList.js
+++ src/SimpleOptionList.js
@@ -1,13 +1,21 @@
 import React from "react";
 
-import SimpleOption from "./SimpleOption";
+import Button from "@material-ui/core/Button";
 
 import "./styles/SimpleOptionList.css";
 
 function SimpleOptionList(props) {
     const { options, handleSelect } = props;
     const buttons = options.map(option => (
-        <SimpleOption key={option.id} {...option} handleSelect={handleSelect} />
+        <Button
+            variant="outlined"
+            key={option.id}
+            data-value={option.id}
+            onClick={handleSelect}
+        >
+            {option.name}
+        </Button>
+        // <SimpleOption key={option.id} {...option} handleSelect={handleSelect} />
     ));
     return <div className="SimpleOptionList">{buttons}</div>;
 }
