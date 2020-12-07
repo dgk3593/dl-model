f7865a4be51ee98f77d5e6cf3dfa36e29debda61
dgk3593 <dgk3593@gmail.com>
2020-12-07 10:34:09 +0700

    dragon animations for testing

---
+++ src/data/animationDragon.js
@@ -0,0 +1,15 @@
+const dragonAni = {
+    d210016_01: [
+        // Agni
+        { name: "Dragon's Roost", code: "DC_d210016_01+DC_d210016_01_90_01" },
+        { name: "Receive Gift", code: "DC_d210016_01+DC_d210016_01_90_04" },
+    ],
+    d210128_01: [
+        // Puppy
+        { name: "Dragon's Roost", code: "DC_d210128_01+DC_d210128_01_90_01" },
+        { name: "Give Back Gifts", code: "DC_d210128_01+DC_d210128_01_90_03" },
+        { name: "Receive Gift", code: "DC_d210128_01+DC_d210128_01_90_04" },
+    ],
+};
+
+export default dragonAni;
