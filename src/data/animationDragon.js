3c264528bd11d531b3d62d85e0affa9cc92ad178
dgk3593 <dgk3593@gmail.com>
2020-12-07 15:57:23 +0700

    3* dragons' animation

--- src/data/animationDragon.js
+++ src/data/animationDragon.js
@@ -1,14 +1,72 @@
 const dragonAni = {
     d210016_01: [
-        // Agni
         { name: "Dragon's Roost", code: "DC_d210016_01+DC_d210016_01_90_01" },
         { name: "Receive Gift", code: "DC_d210016_01+DC_d210016_01_90_04" },
     ],
     d210128_01: [
-        // Puppy
         { name: "Dragon's Roost", code: "DC_d210128_01+DC_d210128_01_90_01" },
         { name: "Give Back Gifts", code: "DC_d210128_01+DC_d210128_01_90_03" },
         { name: "Receive Gift", code: "DC_d210128_01+DC_d210128_01_90_04" },
+    ],
+    d210021_01: [
+        { name: "Dragon's Roost", code: "DC_d210021_01+DC_d210021_01_90_01" },
+        { name: "Receive Gift", code: "DC_d210021_01+DC_d210021_01_90_04" },
+    ],
+    d210022_01: [
+        { name: "Dragon's Roost", code: "DC_d210022_01+DC_d210022_01_90_01" },
+        { name: "Receive Gift", code: "DC_d210022_01+DC_d210022_01_90_04" },
+    ],
+    d210023_01: [
+        { name: "Dragon's Roost", code: "DC_d210023_01+DC_d210023_01_90_01" },
+        { name: "Receive Gift", code: "DC_d210023_01+DC_d210023_01_90_04" },
+    ],
+    d210059_01: [
+        { name: "Dragon's Roost", code: "DC_d210021_01+DC_d210021_01_90_01" },
+        { name: "Receive Gift", code: "DC_d210021_01+DC_d210021_01_90_04" },
+    ],
+    d210060_01: [
+        { name: "Dragon's Roost", code: "DC_d210021_01+DC_d210021_01_90_01" },
+        { name: "Receive Gift", code: "DC_d210021_01+DC_d210021_01_90_04" },
+    ],
+    d210061_01: [
+        { name: "Dragon's Roost", code: "DC_d210021_01+DC_d210021_01_90_01" },
+        { name: "Receive Gift", code: "DC_d210021_01+DC_d210021_01_90_04" },
+    ],
+    d210062_01: [
+        { name: "Dragon's Roost", code: "DC_d210021_01+DC_d210021_01_90_01" },
+        { name: "Receive Gift", code: "DC_d210021_01+DC_d210021_01_90_04" },
+    ],
+    d210063_01: [
+        { name: "Dragon's Roost", code: "DC_d210022_01+DC_d210022_01_90_01" },
+        { name: "Receive Gift", code: "DC_d210022_01+DC_d210022_01_90_04" },
+    ],
+    d210064_01: [
+        { name: "Dragon's Roost", code: "DC_d210022_01+DC_d210022_01_90_01" },
+        { name: "Receive Gift", code: "DC_d210022_01+DC_d210022_01_90_04" },
+    ],
+    d210065_01: [
+        { name: "Dragon's Roost", code: "DC_d210022_01+DC_d210022_01_90_01" },
+        { name: "Receive Gift", code: "DC_d210022_01+DC_d210022_01_90_04" },
+    ],
+    d210066_01: [
+        { name: "Dragon's Roost", code: "DC_d210022_01+DC_d210022_01_90_01" },
+        { name: "Receive Gift", code: "DC_d210022_01+DC_d210022_01_90_04" },
+    ],
+    d210067_01: [
+        { name: "Dragon's Roost", code: "DC_d210023_01+DC_d210023_01_90_01" },
+        { name: "Receive Gift", code: "DC_d210023_01+DC_d210023_01_90_04" },
+    ],
+    d210068_01: [
+        { name: "Dragon's Roost", code: "DC_d210023_01+DC_d210023_01_90_01" },
+        { name: "Receive Gift", code: "DC_d210023_01+DC_d210023_01_90_04" },
+    ],
+    d210069_01: [
+        { name: "Dragon's Roost", code: "DC_d210023_01+DC_d210023_01_90_01" },
+        { name: "Receive Gift", code: "DC_d210023_01+DC_d210023_01_90_04" },
+    ],
+    d210070_01: [
+        { name: "Dragon's Roost", code: "DC_d210023_01+DC_d210023_01_90_01" },
+        { name: "Receive Gift", code: "DC_d210023_01+DC_d210023_01_90_04" },
     ],
 };
 
