aeacf29b19ac7740b51a7e13f61b2c462d4e498c
dgk3593 <dgk3593@gmail.com>
2021-05-19 14:30:10 +0700

    Updates

---
+++ static/js/59.b7b3188d.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpdl-model"]=this["webpackJsonpdl-model"]||[]).push([[59],{315:function(n,t,e){},525:function(n,t,e){"use strict";e.r(t),e.d(t,"default",(function(){return r}));var a=e(94),i=(e(315),e(10));var r=function(n){var t=n.options,e=n.onSelect,r=function(n){var t=n.currentTarget.dataset.value;e(t)},c=t.map((function(n){return Object(i.jsx)(a.a,{variant:"outlined","data-value":n.id,onClick:r,children:n.name},n.id)}));return Object(i.jsx)("div",{className:"SimpleOptionList",children:c})}}}]);
+//# sourceMappingURL=59.b7b3188d.chunk.js.map
