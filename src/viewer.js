ac97648aa9235d274ff9ddf19883df21d6fd7fad
dgk3593 <dgk3593@gmail.com>
2021-10-03 19:53:44 +0700

    double click to fullscreen

--- src/viewer.js
+++ src/viewer.js
@@ -1,12 +1,26 @@
 // @ts-nocheck
 import { DLViewer } from "./dl-viewer";
+import fscreen from "fscreen";
+
 const viewer = new DLViewer();
 viewer.initData();
 viewer.setViewport();
 
 window.addEventListener("resize", () => viewer.setViewport());
+viewer.canvas.addEventListener("dblclick", toggleFullScreen);
 
 window.viewer = viewer;
 window.model = viewer.model;
 
 export default viewer;
+
+function toggleFullScreen() {
+    if (!fscreen.fullscreenEnabled) return;
+
+    if (!fscreen.fullscreenElement) {
+        fscreen.requestFullscreen(viewer.canvas);
+        return;
+    }
+
+    fscreen.exitFullscreen();
+}
