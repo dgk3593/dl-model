d2fe6ba1e19c5859cf81014e998a812139a48eb1
dgk3593 <dgk3593@gmail.com>
2021-10-18 17:44:16 +0700

    Updates

--- assets/LoadSpinner.317f053a.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index.8ee699b6.js";function a({color:r="black"}){const n={};return n["--color"]=r,e("div",{className:"LoadSpinner",style:n,children:e("div",{})})}export{a as L};
---
+++ assets/Stretcher.4318a84c.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index.f969f0f1.js";const t=()=>r("div",{className:"Stretcher"});export{t as S};
