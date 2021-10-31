edf64c2afd423d5649ea745f6e8ff4299e9ad1ad
dgk3593 <dgk3593@gmail.com>
2021-10-31 18:56:33 +0700

    Updates

--- assets/FacePartSelector.9a3369dd.js
+++
@@ -1,1 +0,0 @@
-import{S as e}from"./Selector.08a0c515.js";import{j as r}from"./index.7458866a.js";const s=["Eyes","Mouth","Both"];function n({value:t,onClick:o}){return r(e,{type:"text",options:s,value:t,onClick:o})}export{n as F};
---
+++ assets/Stretcher.4202be47.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index.b07e5622.js";const t=()=>e("div",{className:"Stretcher"});export{t as S};
