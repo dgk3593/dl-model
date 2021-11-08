e707211a9f499701fae9d5178d80980dbe012554
dgk3593 <dgk3593@gmail.com>
2021-11-08 12:50:56 +0700

    Updates

--- assets/Stretcher.41875c0b.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index.6f055f91.js";const t=()=>r("div",{className:"Stretcher"});export{t as S};
---
+++ assets/Stretcher.ef9d12c1.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index.c5f5fee0.js";const t=()=>e("div",{className:"Stretcher"});export{t as S};
