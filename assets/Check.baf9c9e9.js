99feee2f45e05032ee02b8cf91a63aa6ad872031
dgk3593 <dgk3593@gmail.com>
2021-11-16 21:29:27 +0700

    Updates

--- assets/Check.baf9c9e9.js
+++
@@ -1,1 +0,0 @@
-import{j as c}from"./index.5b8c164b.js";import{c as e}from"./createSvgIcon.518d04d2.js";var o=e(c("path",{d:"M9 16.17 4.83 12l-1.42 1.41L9 19 21 7l-1.41-1.41z"}),"Check");export{o as C};
---
+++ assets/Stretcher.ba159dbb.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index.7415e1fb.js";const t=()=>e("div",{className:"Stretcher"});export{t as S};
