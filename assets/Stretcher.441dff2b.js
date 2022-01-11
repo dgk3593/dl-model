f6a7bd382ed664833fffc7094636457dc7ecfb26
dgk3593 <dgk3593@gmail.com>
2022-01-11 14:28:39 +0700

    Updates

--- assets/Stretcher.441dff2b.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index.76e26a48.js";const t=()=>e("div",{className:"Stretcher"});export{t as S};
---
+++ assets/Stretcher.5b0b4f40.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index.4a65589f.js";const t=()=>r("div",{className:"Stretcher"});export{t as S};
