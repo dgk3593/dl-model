7330f6ba34a3509778549dce0b57061406ebfb23
dgk3593 <dgk3593@gmail.com>
2022-06-21 12:04:35 +0700

    ani model icon for mobs

--- src/SceneController/components/DLIcon/ModelIcon.jsx
+++ src/SceneController/components/DLIcon/ModelIcon.jsx
@@ -2,7 +2,7 @@
  * @param {string} id
  */
 export const hasIcon = id =>
-    id && ["b", "c", "d", "h", "r", "w"].includes(id[0]);
+    id && ["b", "c", "d", "h", "r", "w", "e"].includes(id[0]);
 
 function ModelIcon({ modelId, ...others }) {
     const iconPath = hasIcon(modelId) ? `img/icon/${modelId}.png` : "";
