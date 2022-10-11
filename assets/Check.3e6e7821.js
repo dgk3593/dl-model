6300df96ae19c80a0bce958407fa9ab2baef0d6f
dgk3593 <dgk3593@gmail.com>
2022-10-11 10:51:05 +0700

    Updates

---
+++ assets/Check.3e6e7821.js
@@ -0,0 +1,1 @@
+import{o}from"./index.768daeb8.js";import{c}from"./createSvgIcon.924f20ec.js";const e=c(o("path",{d:"M9 16.17 4.83 12l-1.42 1.41L9 19 21 7l-1.41-1.41z"}),"Check");export{e as C};
--- assets/FacePartSelector.51a35f9e.js
+++
@@ -1,1 +0,0 @@
-import{S as e}from"./Selector.1fea1f91.js";import{o as r}from"./index.1696ba87.js";const c=["Eyes","Mouth","Both"];function s({value:t,onClick:o}){return r(e,{type:"text",options:c,value:t,onClick:o})}export{s as F};
