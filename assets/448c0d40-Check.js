c270a842da5f197ae1b005bed542265b0e27debd
dgk3593 <dgk3593@gmail.com>
2023-02-24 11:05:47 +0700

    Updates

--- assets/448c0d40-Check.js
+++
@@ -1,1 +0,0 @@
-import{o}from"./index-14c2c5fa.js";import{c}from"./f09a8fcd-createSvgIcon.js";const e=c(o("path",{d:"M9 16.17 4.83 12l-1.42 1.41L9 19 21 7l-1.41-1.41z"}),"Check");export{e as C};
---
+++ assets/5beb8502-Stretcher.js
@@ -0,0 +1,1 @@
+import{o as t}from"./index-b496b789.js";const e=()=>t("div",{className:"Stretcher"});export{e as S};
