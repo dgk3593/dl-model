cc1d5bb05919e5de73b1adaed84c4b63909d60ae
dgk3593 <dgk3593@gmail.com>
2023-05-07 11:23:57 +0700

    Updates

---
+++ assets/28593f20-useKey.js
@@ -0,0 +1,1 @@
+import{l as s}from"./index-383671ab.js";function r(){const[e,t]=s(0);return[e,()=>t(o=>o+1)]}export{r as u};
--- assets/5c46174c-useToggleState.js
+++
@@ -1,1 +0,0 @@
-import{l as s}from"./index-66e0b68a.js";function n(t=!1){const[e,o]=s(t);return[e,()=>o(a=>!a)]}export{n as u};
