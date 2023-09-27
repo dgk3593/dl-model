8ecfb2a2365789870c296608c372b2ff99f1ecbb
dgk3593 <dgk3593@gmail.com>
2023-09-27 11:30:42 +0700

    Updates

---
+++ assets/07590281-FacePartSelector.js
@@ -0,0 +1,1 @@
+import{S as e}from"./c2b3b93c-Selector.js";import{o as r}from"./index-ed251ef3.js";const c=["Eyes","Mouth","Both"];function s({value:t,onClick:o}){return r(e,{type:"text",options:c,value:t,onClick:o})}export{s as F};
--- assets/eeaff37b-Stretcher.js
+++
@@ -1,1 +0,0 @@
-import{o as t}from"./index-ade8bee3.js";const e=()=>t("div",{className:"Stretcher"});export{e as S};
