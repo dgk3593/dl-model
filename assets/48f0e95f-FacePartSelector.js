c5fa9557d0ecb1699fd97911c7cc31939435f0da
dgk3593 <dgk3593@gmail.com>
2023-10-11 11:57:44 +0700

    Updates

---
+++ assets/48f0e95f-FacePartSelector.js
@@ -0,0 +1,1 @@
+import{S as e}from"./c5add2ca-Selector.js";import{o as r}from"./index-3ffe48be.js";const c=["Eyes","Mouth","Both"];function s({value:t,onClick:o}){return r(e,{type:"text",options:c,value:t,onClick:o})}export{s as F};
---
+++ assets/a5cbb2d2-Check.js
@@ -0,0 +1,1 @@
+import{o}from"./index-3ffe48be.js";import{c}from"./ad62a321-createSvgIcon.js";const e=c(o("path",{d:"M9 16.17 4.83 12l-1.42 1.41L9 19 21 7l-1.41-1.41z"}),"Check");export{e as C};
