eb8f082073f63e356cdab7571dd8638cc75468be
dgk3593 <dgk3593@gmail.com>
2023-11-01 13:19:47 +0700

    Updates

---
+++ assets/be4ce94e-FacePartSelector.js
@@ -0,0 +1,1 @@
+import{S as e}from"./07a85a67-Selector.js";import{o as r}from"./index-3b6aed40.js";const c=["Eyes","Mouth","Both"];function s({value:t,onClick:o}){return r(e,{type:"text",options:c,value:t,onClick:o})}export{s as F};
