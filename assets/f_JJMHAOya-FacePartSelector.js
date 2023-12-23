af54166f02cc74a57765f7ce5f3cab66661e8e02
dgk3593 <dgk3593@gmail.com>
2023-12-23 12:19:23 +0700

    Updates

--- assets/f_JJMHAOya-FacePartSelector.js
+++
@@ -1,1 +0,0 @@
-import{S as e}from"./f_f5YfpCP2-Selector.js";import{u as r}from"./index-uioyZV6Y.js";const c=["Eyes","Mouth","Both"];function s({value:t,onClick:o}){return r(e,{type:"text",options:c,value:t,onClick:o})}export{s as F};
