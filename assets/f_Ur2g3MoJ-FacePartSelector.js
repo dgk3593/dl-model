4ba505fb5be87d229d16749c91ba63e1ec2dd3e6
dgk3593 <dgk3593@gmail.com>
2024-01-03 11:03:48 +0700

    Updates

---
+++ assets/f_Ur2g3MoJ-FacePartSelector.js
@@ -0,0 +1,1 @@
+import{S as e}from"./f_imgss1nP-Selector.js";import{u as r}from"./index-pVnLx2o_.js";const c=["Eyes","Mouth","Both"];function s({value:t,onClick:o}){return r(e,{type:"text",options:c,value:t,onClick:o})}export{s as F};
---
+++ assets/f_b0L7gceP-Stretcher.js
@@ -0,0 +1,1 @@
+import{u as r}from"./index-pVnLx2o_.js";const e=()=>r("div",{className:"Stretcher"});export{e as S};
