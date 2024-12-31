f625cba1ee6ed16201b33f706271792ccc2ce1da
dgk3593 <dgk3593@gmail.com>
2025-01-01 00:28:24 +0700

    Updates

---
+++ assets/f_BxfiWtrE-FacePartSelector.js
@@ -0,0 +1,1 @@
+import{u as e}from"./index-BI2bBlId.js";import{S as r}from"./f_D_L1iPGD-Selector.js";const c=["Eyes","Mouth","Both"];function s({value:t,onClick:o}){return e(r,{type:"text",options:c,value:t,onClick:o})}export{s as F};
--- assets/f_Clfb7_u8-FacePartSelector.js
+++
@@ -1,1 +0,0 @@
-import{u as e}from"./index-ClFHsnvg.js";import{S as r}from"./f_bAvXM9ni-Selector.js";const c=["Eyes","Mouth","Both"];function s({value:t,onClick:o}){return e(r,{type:"text",options:c,value:t,onClick:o})}export{s as F};
