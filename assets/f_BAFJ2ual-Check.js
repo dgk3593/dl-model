a80ed164cc6a016e3f4edfaa71434c9a09cef8c2
dgk3593 <dgk3593@gmail.com>
2025-07-27 00:00:52 +0700

    Updates

--- assets/f_BAFJ2ual-Check.js
+++
@@ -1,1 +0,0 @@
-import{u as o}from"./index-CWevuTuX.js";import{c as r}from"./f_Bc2jFFdE-createSvgIcon.js";const a=r(o("path",{d:"M9 16.17 4.83 12l-1.42 1.41L9 19 21 7l-1.41-1.41z"}));export{a as C};
---
+++ assets/f_BwDXEK_g-FacePartSelector.js
@@ -0,0 +1,1 @@
+import{u as e}from"./index-B2YCL-Yh.js";import{S as r}from"./f_Bi4hmmWw-Selector.js";const c=["Eyes","Mouth","Both"];function s({value:t,onClick:o}){return e(r,{type:"text",options:c,value:t,onClick:o})}export{s as F};
