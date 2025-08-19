5846c3e8547ceb764acafcfd20be813444f50b49
dgk3593 <dgk3593@gmail.com>
2025-08-19 23:52:54 +0700

    Updates

---
+++ assets/f_gdNe-nEL-FacePartSelector.js
@@ -0,0 +1,1 @@
+import{u as e}from"./index-Bxk9gsMd.js";import{S as r}from"./f_FR4YF7oy-Selector.js";const c=["Eyes","Mouth","Both"];function s({value:t,onClick:o}){return e(r,{type:"text",options:c,value:t,onClick:o})}export{s as F};
