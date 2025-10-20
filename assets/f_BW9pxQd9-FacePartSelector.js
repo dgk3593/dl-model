c71cebddd35e251c0a6e59d6db3d683db3ca0278
dgk3593 <dgk3593@gmail.com>
2025-10-21 00:28:47 +0700

    Updates

--- assets/f_BW9pxQd9-FacePartSelector.js
+++
@@ -1,1 +0,0 @@
-import{u as e}from"./index-CXaJUJXq.js";import{S as r}from"./f_DjCiaqaA-Selector.js";const c=["Eyes","Mouth","Both"];function s({value:t,onClick:o}){return e(r,{type:"text",options:c,value:t,onClick:o})}export{s as F};
--- assets/f_D1jkGt3C-index.js
+++
@@ -1,1 +0,0 @@
-import{w as r,d as o,c as e}from"./f_Bg1--fhg-filterConfig.js";const g={"chara-regular":e,"dragon-regular":o,"weapon-regular":r},s=a=>g[a];export{s as g};
