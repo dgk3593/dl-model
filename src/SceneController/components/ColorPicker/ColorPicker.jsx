5d91294d79fc8ee63981c3970aecd55618a73795
dgk3593 <dgk3593@gmail.com>
2026-07-03 10:29:06 +0700

    replace react-color with @uiw/react-color-chrome

--- src/SceneController/components/ColorPicker/ColorPicker.jsx
+++ src/SceneController/components/ColorPicker/ColorPicker.jsx
@@ -4,51 +4,46 @@ import ColorButton from "../ColorButton";
 import commonColors from "./commonColors";
 import "./styles.css";
 
-const ChromePicker = lazy(() =>
-    import("react-color").then(module => ({ default: module.ChromePicker }))
-);
+const ChromePicker = lazy(() => import("@uiw/react-color-chrome"));
 
 function ColorPicker({ initColor = "#cccccc", onSelect }) {
-    const [color, setColor] = useState(initColor);
+  const [color, setColor] = useState(initColor);
 
-    const handleChange = ({ hex }) => setColor(hex);
-    const handleButtonClick = event => setColor(event.target.value);
-    const applyColor = () => onSelect(color);
+  const handleChange = ({ hex }) => setColor(hex);
+  const handleButtonClick = event => setColor(event.target.value);
+  const applyColor = () => onSelect(color);
 
-    const commonColorBtns = (
-        <div className="ColorPicker-common">
-            {commonColors.map(({ name, hex, title }) => (
-                <ColorButton
-                    onClick={handleButtonClick}
-                    value={hex}
-                    color={hex}
-                    title={title}
-                    key={hex}
-                >
-                    {name}
-                </ColorButton>
-            ))}
-        </div>
-    );
+  const commonColorBtns = (
+    <div className="ColorPicker-common">
+      {commonColors.map(({ name, hex, title }) => (
+        <ColorButton
+          onClick={handleButtonClick}
+          value={hex}
+          color={hex}
+          title={title}
+          key={hex}
+        >
+          {name}
+        </ColorButton>
+      ))}
+    </div>
+  );
 
-    return (
-        <div className="ColorPicker">
-            <div className="ColorPicker-select">
-                {commonColorBtns}
+  return (
+    <div className="ColorPicker">
+      <div className="ColorPicker-select">
+        {commonColorBtns}
 
-                <Suspense fallback={null}>
-                    <ChromePicker
-                        color={color}
-                        onChangeComplete={handleChange}
-                    />
-                </Suspense>
-            </div>
+        <Suspense fallback={null}>
+          <ChromePicker color={color} onChange={handleChange} />
+        </Suspense>
+      </div>
 
-            <ColorButton color={color} onClick={applyColor}>
-                Apply
-            </ColorButton>
-        </div>
-    );
+      <ColorButton color={color} onClick={applyColor}>
+        Apply
+      </ColorButton>
+    </div>
+  );
 }
 
 export default ColorPicker;
