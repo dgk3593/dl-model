6fa39143412cf385aaf5219027d047a5574dd836
dgk3593 <dgk3593@gmail.com>
2026-07-03 10:38:04 +0700

    use immer middleware from zustand

--- src/state/index.js
+++ src/state/index.js
@@ -1,18 +1,14 @@
 import { create } from "zustand";
-import { produce } from "immer";
+import { immer } from "zustand/middleware/immer";
 
 import {
-    app,
-    activeModel,
-    modelCatalog,
-    aniSelect,
-    rootModal,
-    chainMaker,
+  app,
+  activeModel,
+  modelCatalog,
+  aniSelect,
+  rootModal,
+  chainMaker,
 } from "./config";
-
-// Turn the set method into an immer proxy
-const immer = config => (set, get, api) =>
-    config(fn => set(produce(fn)), get, api);
 
 const createStore = config => create(immer(config));
 
